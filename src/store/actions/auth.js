4cf412a88ab804f8b5d91a7d97f51750d223df15
Lando <anker.landon@gmail.com>
2018-11-19 19:36:07 -0600

    auto Login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,9 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -41,6 +44,7 @@ export const checkAuthTimeout = experationTime => {
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
     dispatch(authStart());
+    console.log('test', email, password);
     let APIURL = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${APIKEY}`;
     const AuthData = {
       email: email,
@@ -54,16 +58,46 @@ export const auth = (email, password, isSignUp) => {
       .post(APIURL, AuthData)
       .then(res => {
         console.log(res);
+        // save token and expiration Date in Browser storage
+        const expirationDate = new Date(
+          new Date().getTime() + res.data.expiresIn * 1000
+        );
+        localStorage.setItem('token', res.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', res.data.localId);
+
         dispatch(authSuccess(res.data.idToken, res.data.localId));
         dispatch(checkAuthTimeout(res.data.expiresIn));
       })
       .catch(err => {
         console.log(err);
-        dispatch(authFail(err.response.data.error));
+        dispatch(authFail(err));
       });
   };
 };
 
 export const setAuthRedirectPath = path => {
   return { type: actionTypes.SET_AUTH_REDIRECT_PATH, path: path };
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate < new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
+    }
+  };
 };
